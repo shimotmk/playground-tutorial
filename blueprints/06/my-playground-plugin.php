a19ef6f9ba53dd9b2f81cbfdd9495adc7e7386b3
shimotmk <shimotmk1104@gmail.com>
2025-05-04 16:32:41 +0900

    delete

--- blueprints/06/my-playground-plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin name: My Playground Plugin
-*/
-
-function my_custom_plugin() {
-	echo "<h1>Hello from My Playground Plugin!</h1>";
-}
-add_action('admin_notices', 'my_custom_plugin');
