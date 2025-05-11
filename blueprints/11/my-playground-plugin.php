248dea87502e9d530446559259e282ba6043f140
shimotmk <shimotmk1104@gmail.com>
2025-05-11 13:53:55 +0900

    u

--- blueprints/11/my-playground-plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: My Playground Plugin
-*/
-
-function my_playground_plugin() {
-	echo '<h1>Hello, My Playground!</h1>';
-}
-add_action('admin_notices', 'my_playground_plugin');
